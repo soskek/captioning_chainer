ce20a7e6f1e65752b6e2c9cc61ed72e0d228da55
Shun Kiyono <kiyono@ecei.tohoku.ac.jp>
2018-06-12 16:00:06 +0900

    http --> https
    The URL has slightly changed.

--- prepare_scripts/download.sh
+++ prepare_scripts/download.sh
@@ -1,6 +1,6 @@
 mkdir data
 cd data
 
-curl http://cs.stanford.edu/people/karpathy/deepimagesent/flickr8k.zip -o flickr8k.zip
-curl http://cs.stanford.edu/people/karpathy/deepimagesent/flickr30k.zip -o flickr30k.zip
-curl http://cs.stanford.edu/people/karpathy/deepimagesent/coco.zip -o coco.zip
+curl https://cs.stanford.edu/people/karpathy/deepimagesent/flickr8k.zip -o flickr8k.zip
+curl https://cs.stanford.edu/people/karpathy/deepimagesent/flickr30k.zip -o flickr30k.zip
+curl https://cs.stanford.edu/people/karpathy/deepimagesent/coco.zip -o coco.zip
